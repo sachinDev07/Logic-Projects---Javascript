977f472b06ed14029710c01a9461783db5642342
Sachin <sachinkrj077@gmail.com>
2022-09-04 11:22:50 +0500

    first commit

---
+++ Temperature Convertor/script.js
@@ -0,0 +1,29 @@
+
+const calculateTemp = () => {
+
+    const numberTemp = document.getElementById('temp').value;
+
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = tempSelected.options[tempSelected.selectedIndex].value;
+
+    const celToFah = (cel) => {
+        let Fahrenheit =  Math.round((cel * 9/5) + 32);
+        return Fahrenheit;
+    }
+
+    const fahToCel = (fahr) => {
+        let Celsius =  Math.round((fahr - 32) * 5/9);
+        return Celsius;
+    }
+
+
+    let result;
+
+    if(valueTemp == 'cel') {
+        result = celToFah(numberTemp);
+        document.getElementById('result').textContent = `= ${result} Fahrenheit`;
+    }else {
+        result = fahToCel(numberTemp);
+        document.getElementById('result').textContent = `= ${result} Celsius`;
+    }
+}
