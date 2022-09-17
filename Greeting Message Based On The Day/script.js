4ca09770d3f34e41b1b3fd7eb3a0a93056dff91e
Sachin <sachinkrj077@gmail.com>
2022-09-17 21:40:32 +0500

    big change

--- Greeting Message Based On The Day/script.js
+++ Greeting Message Based On The Day/script.js
@@ -1,26 +1,21 @@
 
 let day = new Date();
 console.log(day);
-let t = day.getHours();
-console.log(t);
+let hrs = day.getHours();
 
-let greeting = document.getElementById('greeting');
-if(t >= 00 && t < 12) {
-    let day = "Good Morning";
-    greeting.innerHTML = day;  
+let greet;
+
+if(hrs >= 00 && hrs < 12) {
+    greet = 'Good Morning';
+}
+else if (hrs <= 12 && hrs < 16) {
+    greet = 'Good Agternoon';
+}
+else if ( hrs >= 16 && hrs < 20) {
+    greet = 'Good Evening';
+}
+else {
+    greet = "Good Night";
 }
 
-if(t >= 12 && t < 16) {
-    let day = "Good Afternoon";
-    greeting.innerHTML = day;  
-}
-
-if(t >= 16 && t < 20) {
-    let day = "Good Evening";
-    greeting.innerHTML = day;  
-}
-
-if(t >= 20 && t < 00) {
-    let day = "Good Night";
-    greeting.innerHTML = day;  
-}
+document.getElementById('greeting').innerHTML = greet;
